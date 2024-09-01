a1f0866283a9427b8e785e21b17640593e76d6cb
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-09-01 17:08:22 +0530

    Add files via upload

---
+++ Introductory Problems/Chessboard_and_Queens.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;   // -2^63 to 2^63 - 1
+using ld = long double; // 12 bytes (12*8 bits)
+template <typename T>
+using v = vector<T>;
+template <typename T>
+using vv = vector<vector<T>>;
+template <typename T>
+using us = unordered_set<T>;
+template <typename T>
+using pq = priority_queue<T>;
+template <typename T1, typename T2>
+using p = pair<T1, T2>;
+template <typename T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+template <typename T1, typename T2>
+using um = std::unordered_map<T1, T2>;
+#define pb push_back
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+#define all(x) x.begin(), x.end()
+#define prt(x) cout << x << endl
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define SUM(v) accumulate(all(v), 0LL)
+#define roundUp(a, b) ((a + b - 1) / b)
+#define input(x)      \
+    for (auto &i : x) \
+        cin >> i;
+ll lcm(ll x, ll y) { return x * y / std::__gcd(x, y); }
+#define print(a)          \
+    for (auto x : a)      \
+        cout << x << ' '; \
+    cout << endl;
+
+const int MOD = 1e9 + 7;
+bool isSafe(int i, int j, vector<string> &vs)
+{
+    for (int k = 0; k < 8; k++)
+    {
+        if (k != j)
+        {
+            if (vs[i][k] == '@')
+                return false;
+        }
+    }
+    for (int k = 0; k < 8; k++)
+    {
+        if (k != i)
+        {
+            if (vs[k][j] == '@')
+                return false;
+        }
+    }
+    for (int k = 1; k < 8; k++)
+    {
+        if (i - k >= 0 && j - k >= 0 && vs[i - k][j - k] == '@')
+            return false;
+        if (i + k < 8 && j + k < 8 && vs[i + k][j + k] == '@')
+            return false;
+    }
+    for (int k = 1; k < 8; k++)
+    {
+        if (i - k >= 0 && j + k < 8 && vs[i - k][j + k] == '@')
+            return false;
+        if (i + k < 8 && j - k >= 0 && vs[i + k][j - k] == '@')
+            return false;
+    }
+    return true;
+}
+void f(int i, vector<string> &vs, ll &ans)
+{
+    if (i > 7)
+    {
+        ans++;
+        return;
+    }
+    for (int j = 0; j < 8; j++)
+    {
+        if (vs[i][j] == '.')
+        {
+            bool flag = isSafe(i, j, vs);
+            if (flag)
+            {
+                vs[i][j] = '@';
+                f(i + 1, vs, ans);
+                vs[i][j] = '.';
+            }
+        }
+    }
+}
+void solve()
+{
+    vector<string> vs(8);
+    for (int i = 0; i < 8; i++)
+    {
+        cin >> vs[i];
+    }
+
+    ll ans = 0;
+    f(0, vs, ans);
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
